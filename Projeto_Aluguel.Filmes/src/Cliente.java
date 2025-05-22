04d742dce48c26c358a032883a8389f4624e736a
gumers762 <rodrigo.gumiero09@gmail.com>
2025-05-22 10:48:04 -0300

    Add files via upload

---
+++ Projeto_Aluguel.Filmes/src/Cliente.java
@@ -0,0 +1,64 @@
+public class Cliente extends Pessoa implements IPessoa{
+    private String nome;
+    private int idade;
+    private String CPF;
+    private char sexo;
+    private String email;
+
+    public Cliente(String nome, int idade, String CPF, char sexo, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.CPF = CPF;
+        this.sexo = sexo;
+        this.email = email;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("CPF: " +CPF);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("E-mail: " + email);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Projeto_Aluguel.Filmes/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("José", 32, "153.258.487-14", 'M', "josealberto@gmail.com");
+        Cliente c2 = new Cliente("Maria", 27, "987.123.789-99", 'F', "mariatereza@gmail.com");
+        Cliente c3 = new Cliente("Fábio", 49, "573.917.578-71", 'M', "fabioserelepe@gmail.com");
+        Cliente c4 = new Cliente("Jéssica", 16, "123.234.456-98", 'F', "cl207304@g.unicamp.br");
+
+
+        ArrayList Pessoa = new ArrayList();
+    }
+}
