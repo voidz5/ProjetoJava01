f279db6b8f07f806422b05d020ecca11533969a6
gumers762 <rodrigo.gumiero09@gmail.com>
2025-05-30 11:45:50 -0300

    Funcionario.java

--- Projeto_Aluguel.Filmes/src/Funcionario.java
+++ Projeto_Aluguel.Filmes/src/Funcionario.java
@@ -1,4 +1,4 @@
-public class Funcionario extends Pessoa implements IPessoa{
+public class Funcionario extends Pessoa implements IPessoa {
     private String nome;
     private int idade;
     private int cod;
@@ -6,17 +6,18 @@ public class Funcionario extends Pessoa implements IPessoa{
     private String CPF;
     private int guiche;
 
-    public Funcionario(String nome, int idade, int cod, double salario, String CPF) {
+    public Funcionario(String nome, int idade, int cod, double salario, String CPF, int guiche) {
         this.nome = nome;
         this.idade = idade;
         this.cod = cod;
         this.salario = salario;
         this.CPF = CPF;
+        this.guiche = guiche;
     }
 
     @Override
     public void exibir() {
-        System.out.println("Nome:" + nome);
+        System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         System.out.println("Código: " + cod);
         System.out.println("Salário: " + salario);
@@ -24,51 +25,14 @@ public class Funcionario extends Pessoa implements IPessoa{
         System.out.println("Guichê: " + guiche);
     }
 
+    @Override
     public String getNome() {
-        return nome;
+        return nome;  // Implementando o método getNome()
     }
 
+    // Getters e Setters
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getCPF() {
-        return CPF;
-    }
-
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    public int getCod() {
-        return cod;
-    }
-
-    public void setCod(int cod) {
-        this.cod = cod;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public int getGuiche() {
-        return guiche;
-    }
-
-    public void setGuiche(int guiche) {
-        this.guiche = guiche;
-    }
+    // ... Outros getters e setters
 }
