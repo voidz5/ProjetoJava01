5c7049bfc969273558202cdb1996c8d84420e468
gumers762 <rodrigo.gumiero09@gmail.com>
2025-05-30 11:45:35 -0300

    Cliente.java

--- Projeto_Aluguel.Filmes/src/Cliente.java
+++ Projeto_Aluguel.Filmes/src/Cliente.java
@@ -1,11 +1,11 @@
-public class Cliente extends Pessoa implements IPessoa{
+public class Cliente extends Pessoa implements IPessoa {
     private String nome;
     private int idade;
     private String CPF;
-    private char sexo;
+    private String sexo;
     private String email;
 
-    public Cliente(String nome, int idade, String CPF, char sexo, String email) {
+    public Cliente(String nome, int idade, String CPF, String sexo, String email) {
         this.nome = nome;
         this.idade = idade;
         this.CPF = CPF;
@@ -17,48 +17,19 @@ public class Cliente extends Pessoa implements IPessoa{
     public void exibir() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
-        System.out.println("CPF: " +CPF);
+        System.out.println("CPF: " + CPF);
         System.out.println("Sexo: " + sexo);
         System.out.println("E-mail: " + email);
     }
 
+    @Override
     public String getNome() {
-        return nome;
+        return nome;  // Implementando o método getNome()
     }
 
+    // Getters e Setters
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getCPF() {
-        return CPF;
-    }
-
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // ... Outros getters e setters
 }
